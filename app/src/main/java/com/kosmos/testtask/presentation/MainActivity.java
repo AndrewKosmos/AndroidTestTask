3741b52f6eafd458d0539f2b5ed7fbc0d9a6af7d
AndrewKosmos <EarthPlanetIsGreat@yandex.ru>
2020-01-05 14:31:14 +0400

    basic project structure and depends

--- app/src/main/java/com/kosmos/testtask/presentation/MainActivity.java
+++ app/src/main/java/com/kosmos/testtask/presentation/MainActivity.java
@@ -1,8 +1,10 @@
-package com.kosmos.testtask;
+package com.kosmos.testtask.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.kosmos.testtask.R;
 
 public class MainActivity extends AppCompatActivity {
 
