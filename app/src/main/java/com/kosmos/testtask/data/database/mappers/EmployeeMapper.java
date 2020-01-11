d3c0a66f2aeeec6935a055dbe550f1760f0fc2af
AndrewKosmos <EarthPlanetIsGreat@yandex.ru>
2020-01-11 18:46:01 +0400

    finish mappers

--- app/src/main/java/com/kosmos/testtask/data/database/mappers/EmployeeMapper.java
+++ app/src/main/java/com/kosmos/testtask/data/database/mappers/EmployeeMapper.java
@@ -1,17 +1,20 @@
 package com.kosmos.testtask.data.database.mappers;
 
-import android.text.TextUtils;
-
 import com.kosmos.testtask.data.database.models.EmployeeDbModel;
 import com.kosmos.testtask.domain.models.Employee;
 import com.kosmos.testtask.domain.models.Person;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
 
 public abstract class EmployeeMapper {
 
     public static EmployeeDbModel map(Person person) {
         String name = capitalizeString(person.getName().toLowerCase());
         String lastName = capitalizeString(person.getLastName().toLowerCase());
-        String birthday = person.getBirthday();
+        String birthday = normalizeDate(person.getBirthday());
         String url = person.getAvatarUrl();
         return new EmployeeDbModel(name, lastName, birthday, url);
     }
@@ -27,12 +30,27 @@ public abstract class EmployeeMapper {
     }
 
     private static String capitalizeString(String str) {
-        if (TextUtils.isEmpty(str)) return "";
+        if (str == null || str.length() <= 0) return "";
         return str.substring(0,1).toUpperCase() + str.substring(1);
     }
 
-    /*private static String normalizeDate(String date) {
-
-    }*/
+    private static String normalizeDate(String date) {
+        if (date == null || date.length() <= 0) return "";
+        if (!Pattern.matches("\\d\\d\\d\\d-\\d\\d-\\d\\d", date)) {
+            if (Pattern.matches("\\d\\d-\\d\\d-\\d\\d\\d\\d", date)) {
+                try {
+                    Date date1 = new SimpleDateFormat("dd-MM-yyyy").parse(date);
+                    return new SimpleDateFormat("yyyy-MM-dd").format(date1);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    return "";
+                }
+            }
+            else {
+                return "";
+            }
+        }
+        return date;
+    }
 
 }
